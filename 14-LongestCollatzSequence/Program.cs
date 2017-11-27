c5f98f09c44276283126b66a706ffa2b427e7b35
Blake Steel <BlakeMSteel@gmail.com>
2017-11-27 17:58:45 -0500

    Longest Collatz Sequence

---
+++ 14-LongestCollatzSequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LongestCollatzSequence
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int largestCollatzNumber = 1;
+            int longestCollatzCount = 1;
+            for (int i = 1; i <= 1000000; i++) {
+                int collatzCount = 1;
+                long collatzNumber = i;
+                while (collatzNumber != 1){
+                    if (collatzNumber % 2 == 0) {
+                        collatzNumber /= 2;
+                        collatzCount++;
+                    }
+                    else {
+                        collatzNumber = collatzNumber * 3 + 1;
+                        collatzCount++;
+                    }
+                }
+                if (collatzCount >= longestCollatzCount)
+                {
+                    largestCollatzNumber = i;
+                    longestCollatzCount = collatzCount;
+                    Console.WriteLine("New longest collatz number: " + largestCollatzNumber + ", " + longestCollatzCount);
+                }
+            }
+        }
+    }
+}
